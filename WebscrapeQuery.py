fe7774a2d2252729f332c59018fdead8a4af01cb
Clinton Ali <aliakolade@gmail.com>
2023-10-26 01:17:03 +0100

    Initial commit

---
+++ WebscrapeQuery.py
@@ -0,0 +1,13 @@
+# niches: "IT OR Information Technology", "Engineering", "Teaching", "Health Care", "Social Care", "Health and Safety"
+niches = ["IT OR Information Technology", "Engineering", "Teaching"]
+
+scraped = ["@gmail.com", "@yahoo.com", ".co.uk", "+44", "Tel"]
+
+# Sites: "uk.linkedin.com/in", "uk.linkedin.com/company", "indeed.com"
+sites = ["uk.linkedin.com/company"]
+location = "UK OR United Kingdom OR England"
+
+for site in sites:
+    for niche in niches:
+        for scrape in scraped:
+            print("\"" + scrape + "\" + " + niche + " + " + location + " + site:" + site)
